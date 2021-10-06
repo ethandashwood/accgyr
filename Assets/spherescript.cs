216be97c9414967a14351e7ad5f010db5407342f
ethandashwood <72386500+ethandashwood@users.noreply.github.com>
2021-10-06 11:18:16 +0100

    Initial commit

---
+++ Assets/spherescript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spherescript : MonoBehaviour
+{
+
+    public bool isFlat = true;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        if (isFlat)
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+
+        rigid.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+    }
+}
